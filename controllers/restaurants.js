bb738d910b5955ac3140edbf9c23c55845901ebb
Sarah Ließ <sarah.liess@hotmail.de>
2021-09-17 09:49:17 +0200

    added populate method

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -2,7 +2,9 @@ const Restaurant = require("../models/Restaurant");
 
 const getRestaurants = async (req, res) => {
   try {
-    const restaurants = await Restaurant.find();
+    const restaurants = await Restaurant.find()
+      .populate("city")
+      .populate("tag");
     console.log(restaurants);
     res.json({
       success: true,
@@ -17,7 +19,9 @@ const getRestaurants = async (req, res) => {
 const getRestaurant = async (req, res) => {
   try {
     const { id } = req.params;
-    const restaurant = await Restaurant.findById(id);
+    const restaurant = await Restaurant.findById(id)
+      .populate("city")
+      .populate("tag");
 
     res.json({
       msg: `show restaurant with id ${id}`,
